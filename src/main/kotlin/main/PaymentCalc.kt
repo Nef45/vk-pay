2914f77f52bf95153f94d1237b41e3f931157c60
Nef45 <nef4571@gmail.com>
2022-03-23 22:50:46 +0900

    (Added) Unit testing and minor changes in functions logic

--- src/main/kotlin/main/PaymentCalc.kt
+++ src/main/kotlin/main/PaymentCalc.kt
@@ -6,7 +6,8 @@ const val COMMISSION_PCT_MAESTRO = 0.006
 const val COMMISSION_PCT_MASTERCARD = 0.006
 const val COMMISSION_PCT_MIR = 0.0075
 const val COMMISSION_PCT_VISA = 0.0075
-const val COMMISSION_PCT_VKPAY = 0.0
+
+const val COMMISSION_VKPAY = 0.0
 
 const val MIN_COMMISSION_SUM_MAESTRO = 20_00
 const val MIN_COMMISSION_SUM_MASTERCARD = 20_00
@@ -24,24 +25,10 @@ const val VKPAY_LIMIT_SUM_MONTH = 40_000_00
 fun main() {
     val transferSum = 10_000_00
 
-    //Mastercard & Maestro
-    payCommission(cardType = "Maestro", transferSum = transferSum)
-    payCommission("Maestro", 70_000_00, transferSum)
-    payCommission("Mastercard", 595_000_00, transferSum)
+    println(payCommission(cardType = "Maestro", transferSum = transferSum))
+    println(payCommission("Maestro", 70_000_00, transferSum))
+    println(payCommission("Mastercard", 595_000_00, transferSum))
 
-    println()
-
-    //Mir & Visa
-    payCommission(transferSum = transferSum, cardType = "Visa")
-    payCommission(transferSum = 100_00, cardType = "Visa")
-    payCommission("Mir", 591_000_00, transferSum)
-
-    println()
-
-    //VK Pay
-    payCommission(transferSum = transferSum)
-    payCommission(transferSum = 16_000_00)
-    payCommission(transferSum = transferSum, previousPaymentsSum = 35_000_00)
 }
 
 fun isBeyondLimits(cardType: String, previousPaymentsSum: Int, transferSum: Int): Boolean {
@@ -58,9 +45,7 @@ fun isBeyondLimits(cardType: String, previousPaymentsSum: Int, transferSum: Int)
                     transferSum in 0..VKPAY_LIMIT_SUM_DAY -> false
             else -> true
         }
-        else -> {
-            true
-        }
+        else -> true
     }
 }
 
@@ -84,24 +69,20 @@ fun calculateCommission(cardType: String, previousPaymentsSum: Int, transferSum:
                     in 0..MIN_COMMISSION_SUM_VISA -> MIN_COMMISSION_SUM_VISA
                     else -> transferSum * COMMISSION_PCT_VISA
                 }
-                "VK Pay" -> transferSum * COMMISSION_PCT_VKPAY
-                else -> -1
+                "VK Pay" -> COMMISSION_VKPAY
+                else -> throw RuntimeException()
             }
         } else {
-            -2
+            -1
         }
     return commissionSum.toDouble().roundToInt()
 }
 
-fun payCommission(cardType: String = "VK Pay", previousPaymentsSum: Int = 0, transferSum: Int) {
+fun payCommission(cardType: String = "VK Pay", previousPaymentsSum: Int = 0, transferSum: Int): String {
     val commissionSum = calculateCommission(cardType, previousPaymentsSum, transferSum)
-    when (commissionSum) {
-        -1 -> println("Unknown payment system")
-        -2 -> println(
-            """
-            You have reached the transfer limit! Try to change the transferred sum and try again.
-            """.trimIndent()
-        )
-        else -> println("Commission sum: ${commissionSum / 100} rub. ${commissionSum % 100} kop.")
+    val string = when (commissionSum) {
+        -1 -> "You have reached the transfer limit or are using unknown payment system"
+        else -> "Commission sum: ${commissionSum / 100} rub. ${commissionSum % 100} kop."
     }
+    return string
 }
---
+++ src/test/kotlin/main/PaymentCalcKtTest.kt
@@ -0,0 +1,368 @@
+package main
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import kotlin.math.roundToInt
+
+class PaymentCalcKtTest {
+
+    // =======================
+    //isBeyondLimits for cards
+    // =======================
+    @Test
+    fun isBeyondLimits_returnFalse_when_transferSumIsInLimits_for_cards_TT() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH - transferSum
+        val expectedResult = false
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_cards_TF() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY + 100_00
+        val previousPaymentsSum = CARDS_LIMIT_SUM_DAY
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_cards_FT() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_cards_FF() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY + 100_00
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_cards_TF_negative() {
+        val cardType = "Mastercard"
+        val transferSum = -CARDS_LIMIT_SUM_DAY
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_cards_FT_negative() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY
+        val previousPaymentsSum = -CARDS_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //isBeyondLimits for VKPay
+    // =======================
+    @Test
+    fun isBeyondLimits_returnFalse_when_transferSumIsInLimits_for_vkPay_TT() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY
+        val previousPaymentsSum = VKPAY_LIMIT_SUM_MONTH - transferSum
+        val expectedResult = false
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_vkPay_TF() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY + 100_00
+        val previousPaymentsSum = VKPAY_LIMIT_SUM_DAY
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_vkPay_FT() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY
+        val previousPaymentsSum = VKPAY_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_vkPay_FF() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY + 100_00
+        val previousPaymentsSum = VKPAY_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_vkPay_TF_negative() {
+        val cardType = "VK Pay"
+        val transferSum = -VKPAY_LIMIT_SUM_DAY
+        val previousPaymentsSum = VKPAY_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun isBeyondLimits_returnTrue_when_transferSumIsTooBig_for_vkPay_FT_negative() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY
+        val previousPaymentsSum = -VKPAY_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //isBeyondLimits for unknown card type
+    // =======================
+    @Test
+    fun isBeyondLimits_returnTrue_when_cardType_Unknown() {
+        val cardType = "XXX"
+        val transferSum = CARDS_LIMIT_SUM_DAY
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH
+        val expectedResult = true
+
+        val actualResult = isBeyondLimits(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+
+    // =======================
+    //calculateCommission for Mastercard within limits
+    // =======================
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_meetsSpecialOfferConditions_Mastercard() {
+        val cardType = "Mastercard"
+        val transferSum = MAX_COMMISSION_FREE_SUM_MASTERCARD - 1
+        val previousPaymentsSum = MAX_COMMISSION_FREE_SUM_MASTERCARD - transferSum
+        val expectedResult = 0
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_beyondSpecialOfferConditions_Mastercard() {
+        val cardType = "Mastercard"
+        val transferSum = MAX_COMMISSION_FREE_SUM_MASTERCARD - 1
+        val previousPaymentsSum = MAX_COMMISSION_FREE_SUM_MASTERCARD - transferSum + 1
+        val expectedResult = (transferSum * COMMISSION_PCT_MASTERCARD + MIN_COMMISSION_SUM_MASTERCARD).toDouble().roundToInt()
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //calculateCommission for Maestro within limits
+    // =======================
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_meetsSpecialOfferConditions_Maestro() {
+        val cardType = "Maestro"
+        val transferSum = MAX_COMMISSION_FREE_SUM_MAESTRO - 1
+        val previousPaymentsSum = MAX_COMMISSION_FREE_SUM_MAESTRO - transferSum
+        val expectedResult = 0
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_beyondSpecialOfferConditions_Maestro() {
+        val cardType = "Maestro"
+        val transferSum = MAX_COMMISSION_FREE_SUM_MAESTRO - 1
+        val previousPaymentsSum = MAX_COMMISSION_FREE_SUM_MAESTRO - transferSum + 1
+        val expectedResult = (transferSum * COMMISSION_PCT_MAESTRO + MIN_COMMISSION_SUM_MAESTRO).toDouble().roundToInt()
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //calculateCommission for Mir within limits
+    // =======================
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_minimumCommission_Mir() {
+        val cardType = "Mir"
+        val transferSum = ((MIN_COMMISSION_SUM_MIR - 1) / COMMISSION_PCT_MIR).roundToInt()
+        val previousPaymentsSum = 0
+        val expectedResult = MIN_COMMISSION_SUM_MIR
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_normalCommission_Mir() {
+        val cardType = "Mir"
+        val transferSum = ((MIN_COMMISSION_SUM_MIR + 100_00) / COMMISSION_PCT_MIR).roundToInt()
+        val previousPaymentsSum = 0
+        val expectedResult = (transferSum * COMMISSION_PCT_MIR).roundToInt()
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //calculateCommission for Visa within limits
+    // =======================
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_minimumCommission_Visa() {
+        val cardType = "Visa"
+        val transferSum = ((MIN_COMMISSION_SUM_VISA - 1) / COMMISSION_PCT_VISA).roundToInt()
+        val previousPaymentsSum = 0
+        val expectedResult = MIN_COMMISSION_SUM_VISA
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_normalCommission_Visa() {
+        val cardType = "Visa"
+        val transferSum = ((MIN_COMMISSION_SUM_VISA + 100_00) / COMMISSION_PCT_VISA).roundToInt()
+        val previousPaymentsSum = 0
+        val expectedResult = (transferSum * COMMISSION_PCT_VISA).roundToInt()
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //calculateCommission for VK Pay within limits
+    // =======================
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_cardType_vkPay() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY - 1
+        val previousPaymentsSum = 0
+        val expectedResult = COMMISSION_VKPAY.roundToInt()
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    // =======================
+    //calculateCommission for payment systems when transfer sum is beyond limits / with unknown card type
+    // =======================
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_unknownCardType() {
+        val cardType = "XXX"
+        val transferSum = VKPAY_LIMIT_SUM_DAY
+        val previousPaymentsSum = 0
+        val expectedResult = -1
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+    @Test
+    fun calculateCommission_returnCorrectCommissionSum_when_beyondLimits() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY + 1
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH + transferSum
+        val expectedResult = -1
+
+        val actualResult = calculateCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+
+    // =======================
+    //payCommission within/beyond limits
+    // =======================
+    @Test
+    fun payCommission_returnCorrectStringWithCommissionSum_withinLimits() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH - transferSum
+        val commissionSum = calculateCommission(cardType, previousPaymentsSum, transferSum)
+        val expectedResult = "Commission sum: ${commissionSum / 100} rub. ${commissionSum % 100} kop."
+
+        val actualResult = payCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun payCommission_returnCorrectStringWithCommissionSum_beyondLimits() {
+        val cardType = "Mastercard"
+        val transferSum = CARDS_LIMIT_SUM_DAY + 1
+        val previousPaymentsSum = CARDS_LIMIT_SUM_MONTH + transferSum
+        val commissionSum = calculateCommission(cardType, previousPaymentsSum, transferSum)
+        val expectedResult = "You have reached the transfer limit or are using unknown payment system"
+
+        val actualResult = payCommission(cardType, previousPaymentsSum, transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun payCommission_returnCorrectStringWithCommissionSum_default() {
+        val cardType = "VK Pay"
+        val transferSum = VKPAY_LIMIT_SUM_DAY
+        val previousPaymentsSum = 0
+        val commissionSum = calculateCommission(cardType, previousPaymentsSum, transferSum)
+        val expectedResult = "Commission sum: ${commissionSum / 100} rub. ${commissionSum % 100} kop."
+
+        val actualResult = payCommission(transferSum = transferSum)
+
+        assertEquals(expectedResult, actualResult)
+    }
+}
